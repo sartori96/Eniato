b732903a3936583ee4ebd1f4378549b1e9e7269f
Gabriel V Sartori <gabrielsartori96@gmail.com>
2021-11-25 20:36:18 -0300

    Att de 25/11

--- ext/crawler.py
+++ ext/crawler.py
@@ -1,14 +1,18 @@
 import requests
+import re
 from flask import Flask
 from bs4 import BeautifulSoup
 
-provincias = requests.get("https://www.google.com/")
+provincias = requests.get("https://pc.saiteichingin.info/table/page_list_nationallist.php")
 
 src = provincias.content
 
 soup = BeautifulSoup(src, 'html.parser')
 
-todosA = soup.find_all('a')
+mostra = soup.find('div', attrs={'class': 'industryTableArea_list'})
 
-print(todosA)
-#print(src)
+#target_content = soup.find('td', attrs={'class': 'money'})
+
+#dt = target_content.find('td').get_text()
+
+print(mostra)
