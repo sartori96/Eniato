c3447a679b80231be810a4c1e393fc9b531a4fc0
Gabriel V Sartori <gabrielsartori96@gmail.com>
2021-11-30 19:08:26 -0300

    Att de 30/11

--- dependencias/ext/site/crawler.py
+++ dependencias/ext/site/crawler.py
@@ -1,14 +1,13 @@
 import requests
-from flask import Flask
 from bs4 import BeautifulSoup
 
 soup = BeautifulSoup(requests.get("https://pc.saiteichingin.info/table/page_list_nationallist.php").content, 'html.parser')
 
 mostra = soup.find('div', attrs={'class': 'industryTableArea_list'})
 
-lista =[]
+lista ={}
 for tr in mostra.select('tr'):
-    lista.append(tr.text.strip())
-
-lista.pop(0)
-print(soup)
+    lista.update({tr.find('a'): tr.find('td', attrs={'class':'money'})})
+    
+print(lista)
+    
