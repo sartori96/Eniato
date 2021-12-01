7ea68bef00ae04ffbf476712b7955fb12259b3d0
Gabriel V Sartori <gabrielsartori96@gmail.com>
2021-11-30 22:32:58 -0300

    limpeza de código

--- dependencias/ext/site/crawler.py
+++ dependencias/ext/site/crawler.py
@@ -8,30 +8,7 @@ def crawler():
 
     mostra = soup.find('div', attrs={'class': 'industryTableArea_list'})
 
-    #lista ={}
-    #for tr in mostra.select('tr'):
-    #    lista.update({tr.find('a'): tr.find('td', attrs={'class':'money'})})
-    #    
-    #
-
-
     lista =[]
     for tr in mostra.select('td', attrs={'a'}):
         lista.append(tr.text.strip())
-
-
-
-
-
-    #alt
-    #lista =[]
-    #for tr in mostra.select('tr'):
-    #    lista.append(tr.text.strip())
-
-    #lista.pop(0)
-    #print(lista)
-
-    #for x in lista:
-    #    print (x)
-    print(lista)
     return lista
